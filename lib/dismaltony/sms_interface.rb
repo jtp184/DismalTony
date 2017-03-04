a68ed87c263494e5b7a7499a13c03c0da11f32ff
Justin Piotroski <justinpiotroski@justinsckintosh.attlocal.net>
2017-03-03 17:38:40 -0800

    SMSInterface now uses ENV variables

--- lib/dismaltony/sms_interface.rb
+++ lib/dismaltony/sms_interface.rb
@@ -5,8 +5,8 @@ module DismalTony
     attr_accessor :destination
 
     def initialize(dest = '')
-      twilio_account_sid = 'AC6188347d1562eb480eec4771f7628f9a'
-      twilio_auth_token = '7b56edc33f1e667929607257056b37eb'
+      twilio_account_sid = ENV['twilio_account_sid']
+      twilio_auth_token = ENV['twilio_auth_token']
       @client = Twilio::REST::Client.new twilio_account_sid, twilio_auth_token
       @destination = dest
     end
