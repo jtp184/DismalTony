922074843486797816aa7ab7e0b842acf514a4b8
Justin Piotroski <justin.piotroski@gmail.com>
2017-08-26 20:36:46 -0700

    Cosmetic changes to QueryResolver as I prune through it

--- lib/dismaltony/query_resolver.rb
+++ lib/dismaltony/query_resolver.rb
@@ -15,38 +15,38 @@ module DismalTony
         )
     end
 
-    def query!(str = '', user_identity = DismalTony::UserIdentity::DEFAULT, silent = false)
-      responded = []
-      user_cs = user_identity.conversation_state
-      post_handled = DismalTony::HandledResponse.new
+  def query!(str = '', user_identity = DismalTony::UserIdentity::DEFAULT, silent = false)
+    responded = []
+    user_cs = user_identity.conversation_state
+    post_handled = DismalTony::HandledResponse.new
 
-      if ret = user_cs.next_handler
-        handle = (handlers.select { |hand| hand.new(self).handler_name == ret.to_s }).first.new(self)
-        handle.merge_data(user_cs.data_packet)
-        post_handled = if user_cs.next_method == 'index'
-         handle.activate_handler! str, user_identity
-       else
-        ret_method = user_cs.next_method
-        post_handled = if handle.respond_to? ret_method
-          if user_cs.next_args
-            handle.method(ret_method.to_sym).call(user_cs.next_args.split(', ') + [str, user_identity])
-          else
-            handle.method(ret_method.to_sym).call(str, user_identity)
-          end
-        else
-          DismalTony::HandledResponse.finish "~e:frown I'm sorry, there appears to be a problem with that program"
-        end
-      end
+    if ret = user_cs.next_handler
+      handle = (handlers.select { |hand| hand.new(self).handler_name == ret.to_s }).first.new(self)
+      handle.merge_data(user_cs.data_packet)
+      post_handled = if user_cs.next_method == 'index'
+                       handle.activate_handler! str, user_identity
+                     else
+                      ret_method = user_cs.next_method
+                      post_handled = if handle.respond_to? ret_method
+                                      if user_cs.next_args
+                                        handle.method(ret_method.to_sym).call(user_cs.next_args.split(', ') + [str, user_identity])
+                                      else
+                                        handle.method(ret_method.to_sym).call(str, user_identity)
+                                      end
+                                    else
+                                      DismalTony::HandledResponse.finish "~e:frown I'm sorry, there appears to be a problem with that program"
+                                    end
+                      end
     else
       post_handled = if responded.empty?
-        DismalTony::HandledResponse.error
-      elsif responded.length == 1
-        responded.first.activate_handler! str, user_identity
-      elsif responded.any? { |hand| hand.handler_name == 'explain-handler' }
-        (responded.select { |hand| hand.handler_name == 'explain-handler' }).first.activate_handler! str, user_identity
-      else
-        responded.first.activate_handler! str, user_identity
-      end
+                      DismalTony::HandledResponse.error
+                    elsif responded.length == 1
+                      responded.first.activate_handler! str, user_identity
+                    elsif responded.any? { |hand| hand.handler_name == 'explain-handler' }
+                      (responded.select { |hand| hand.handler_name == 'explain-handler' }).first.activate_handler! str, user_identity
+                    else
+                      responded.first.activate_handler! str, user_identity
+                    end
     end
     post_handled
   end
