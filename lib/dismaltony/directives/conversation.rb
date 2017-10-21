5fb9fd9ce826a93d20da23d2de70fbd8da72f803
Justin Piotroski <justin.piotroski@gmail.com>
2017-10-20 21:13:28 -0700

    Hunted a bug down in a Directive where it was out of scope

--- lib/dismaltony/directives/conversation.rb
+++ lib/dismaltony/directives/conversation.rb
@@ -1,27 +1,29 @@
-class GreetingDirective < DismalTony::Directive
-	set_name :hello
-	set_group :fun
+module DismalTony::Directives
+	class GreetingDirective < DismalTony::Directive
+		set_name :hello
+		set_group :fun
 
-	add_criteria do |qry|
-		qry << must { |q| q.contains?(/hello/i) || q.contains?(/greetings/i) }
-		qry << should { |q| !q['rel', 'discourse'].empty?}
-		qry << should { |q| !q['xpos', 'UH'].empty?}
-	end
+		add_criteria do |qry|
+			qry << must { |q| q.contains?(/hello/i) || q.contains?(/greetings/i) }
+			qry << should { |q| !q['rel', 'discourse'].empty?}
+			qry << should { |q| !q['xpos', 'UH'].empty?}
+		end
 
-	def run
-		if query =~ /how are you/i
-			DismalTony::HandledResponse.finish("~e:thumbsup I'm doing well!")
-		else
-			DismalTony::HandledResponse.finish([
-				'~e:wave Hello!',
-				'~e:smile Greetings!',
-				'~e:rocket Hi!',
-				"~e:star Hello, #{query.user['nickname']}!",
-				'~e:snake Greetings!',
-				'~e:cat Hi!',
-				"~e:octo Greetings, #{query.user['nickname']}!",
-				'~e:spaceinvader Hello!'
-				].sample)
+		def run
+			if query =~ /how are you/i
+				DismalTony::HandledResponse.finish("~e:thumbsup I'm doing well!")
+			else
+				DismalTony::HandledResponse.finish([
+					'~e:wave Hello!',
+					'~e:smile Greetings!',
+					'~e:rocket Hi!',
+					"~e:star Hello, #{query.user['nickname']}!",
+					'~e:snake Greetings!',
+					'~e:cat Hi!',
+					"~e:octo Greetings, #{query.user['nickname']}!",
+					'~e:spaceinvader Hello!'
+					].sample)
+			end
 		end
 	end
 end
