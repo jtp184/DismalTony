bd5092723b8bcf3a7761d176d263846eafacf181
Justin Piotroski <justin.piotroski@gmail.com>
2018-03-30 18:26:41 -0700

    Added the tony exe and updated data_storage to latest version

--- lib/dismaltony/data_storage.rb
+++ lib/dismaltony/data_storage.rb
@@ -12,10 +12,10 @@ module DismalTony # :nodoc:
     attr_reader :users
 
     # Initializes an empty store, and merges +args+ with #opts
-    def initialize(**args)
+    def initialize(args={})
       @opts = {}
-      @opts.merge!(args) unless args.nil?
-      @users = []
+      @opts.merge!(args.fetch(:opts) { {} })
+      @users = args.fetch(:users) { [] }
     end
 
     # Creates a new UserIdentity object with UserIdentity.user_data set to +opts+
@@ -41,35 +41,50 @@ module DismalTony # :nodoc:
     end
   end
 
+  # Represents storing the DataStore to disk in a YAML file. A relatively simple wrapper around
+  # the internal DataStore that handles loading and saving the YAML.
   class YAMLStore
+    # The location of the store on disk
     attr_reader :filepath
+    # The wrapped DataStore object
     attr_reader :data_store
 
+    # Args takes hooks for :filepath and :data_store, and autopopulates them if not present.
     def initialize(**args)
-      @filepath = args[:filepath]
-      @data_store = args[:data_store] || DataStore.new
+      @filepath = args.fetch(:filepath) { "./tony.yml" }
+      @data_store = args.fetch(:data_store) { DataStore.new }
     end
 
-    def self.load_from(fp = './')
+    # Takes a filepath +fp+ and reads in that YAMLStore.
+    def self.load_from(fp = './tony.yml')
       loaded = self.new(filepath: fp, data_store: Psych.load(File.read(fp)))
       loaded.opts[:env_vars]&.each_pair { |key, val| ENV[key] = val }
       loaded
     end
 
+    # Creates a new YAMLStore at the filepath +fp+, and saves it to disk as well.
     def self.create_at(fp)
       new_store = self.new(filepath: fp)
       new_store.save
     end
 
+    # Kickback function. Ignores +_handled+ and saves to disk after every query.
     def on_query(_handled)
       save
     end
 
+    # Overwrites the file at +filepath+ with the current data.
     def save
       File.open(filepath, 'w+') { |f| f << Psych.dump(data_store) }
       self
     end
 
+    # Forcibly reloads the store
+    def load
+      load_from(filepath)
+    end
+
+    # Checks the internal +datastore+ to see if it responds to +name+, and passes the +params+ along.
     def method_missing(name, *params)
       @data_store.respond_to?(name) ? @data_store.method(name).(*params) : super
     end
