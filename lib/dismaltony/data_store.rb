5bf708b35eb991c1facfb7513cdd32a8916bb724
Justin Piotroski <justin.piotroski@gmail.com>
2018-06-07 21:39:52 -0700

    moved the redis store into its own file for now.

--- lib/dismaltony/data_store.rb
+++ lib/dismaltony/data_store.rb
@@ -1,5 +1,4 @@
 require 'psych' # :nodoc:
-require 'redis'
 
 module DismalTony # :nodoc:
   # Represents the collection of options and users that comprise the VI's memory.
@@ -100,41 +99,6 @@ module DismalTony # :nodoc:
     # Checks the internal +datastore+ to see if it responds to +name+, and passes the +params+ along.
     def method_missing(name, *params)
       @data_store.respond_to?(name) ? @data_store.method(name).(*params) : super
-    end
-  end
-
-  class RedisStore
-    attr_reader :redis_options
-
-    def initialize(opts={})
-      @redis_options = opts.fetch(:redis_options) { {} }
-      @redis_client = Redis.new(redis_options) 
-      @identifier = opts.fetch(:identifier) { 'dismaltony' }
-    end
-
-    def new_user(opts={})
-      u_ident = 
-      opts.each do |ky, vl|
-        @redis_client.pipeline do
-          @redis_client.set("#{ @identifier }:users:#{u_ident}:options:#{ky}", vl)
-        end
-      end
-    end
-
-    def on_query
-
-    end
-
-    def select_user(&block)
-
-    end
-
-    def delete_user(user)
-
-    end
-
-    def store_data(slug)
-
     end
   end
 end
