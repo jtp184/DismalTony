7fbf2b68d23a1acb23f45a5ba05cb49cad911563
Justin Piotroski <justin.piotroski@gmail.com>
2018-07-09 17:04:35 -0700

    Extensive modification to the scheduler functionality, which now works to enqueue ScheduleEvents and run them

--- lib/dismaltony/scheduler.rb
+++ lib/dismaltony/scheduler.rb
@@ -1,45 +1,56 @@
 module DismalTony #:nodoc:
-  # The resolver for the Scheduling system
-  # Loads events from a DataStore object and handles their execution.
+  # The class responsible for storing and retrieving ScheduleEvent objects
   class Scheduler
-    # Configures using +opts+
-    # * :vi - The VIBase to use to execute the events
-    # * :failure_method - a Proc (or something with a #call) to run once per failed event
-    def initialize(**opts)
-      @vi = (opts[:vi] || DismalTony::VIBase.new)
-      @failure_method = (opts[:failure_method] || nil)
+    def self.new_event(init_opts)
+      ino = {}
+      ino[:time] = init_opts.fetch(:time) { Time.now }
+      ino[:query] = init_opts.fetch(:query) { '' }
+      ino[:user_id] = init_opts.fetch(:user_id) { DismalTony.().user[:uuid] }
+      ino[:opts] = init_opts.fetch(:opts) { {} }
+      e = ScheduleEvent.new(ino)
+      store_event(e)
+      e
     end
 
-    # Syntactic sugar for <tt>@vi.data_store</tt>
-    def data_store
-      @vi.data_store
+    def self.store_event(e)
+      DismalTony.().data_store.store_data(directive: :core_scheduler, key: "Event:#{e.event_id}", value: e)
     end
 
-    # Loads the events on #data_store
-    def events
-      data_store.load_events
+    def self.load_events
+      DismalTony.().data_store.read_data(:core_scheduler)
     end
 
-    # Main resolution method.
-    # Iterates through the events, and handles them depending on event state.
-    # Deletes successfully finished or failed events, calling <tt>@failure_method</tt> if it exists
-    def execute
-      events.each do |event|
-        case event.state
-        when :waiting
-          event.run(@vi) if event.ready?
-        when :success
-          events.delete(event)
-        when :failure
-          failure_method.call if @failure_method
-          events.delete(event)
-        end
+    def self.ready_events
+      e = load_events
+      e.select(&:ready?)
+    end
+
+    def self.call
+      e = load_events
+      r = e.map(&:call)
+      r.each do |f|
+        DismalTony.().data_store.update_data(directive: :core_scheduler, key: "Event:#{f.event_id}", value: f)
+      end
+      r
+    end
+
+    def self.wait
+      loop do
+        call
+        prune_events
       end
     end
 
-    # Shortcut method to append the event to the data_store
-    def <<(other)
-      data_store.add_event(other)
+    def self.events_for_user(uid)
+      e = load_events
+      e.select { |f| f.user_id == uid }
+    end
+
+    def self.prune_events
+      e = load_events
+      e.each do |f|
+        DismalTony.().data_store.delete_data(:core_scheduler, "Event:#{f.event_id}") if f.finished?
+      end
     end
   end
 
@@ -49,29 +60,41 @@ module DismalTony #:nodoc:
 
     # The Time object representing when to run this event
     attr_reader :time
-    # The ConversationState object representing a state to put the UserIdentity in, if dropping into a handler chain.
-    attr_reader :conversation_state
     # The state of the handler itself. either <tt>:waiting, :success, :failure</tt>
     attr_reader :state
     # A String which resolves to a query to put to the VI when the event runs if running a timed query
     attr_reader :query
-    # The UserIdentity of the user who is invoking the event.
+    # The uuid of the user who is invoking the event.
     attr_reader :user_id
+    # A uuid for this event
+    attr_reader :event_id
+    # An array of options for the event
+    attr_reader :opts
 
     # Delegates time related functions to the #time object
     def_delegators :@time, :<, :>, :>=, :<=, :monday?, :tuesday?, :wednesday?, :thursday?, :friday?, :saturday?, :sunday?, :day, :sec, :hour, :day, :month, :year
 
-    # +opts+ initialization defaults
+    # +args+ options
     # * :time - Defaults to Time.now
     # * :state - Defaults to :waiting
     # * :query - Defaults to nil
-    # * :user_id - Defaults to the Default User
-    def initialize(**opts)
-      @time = (opts[:time] || Time.now)
+    # * :user_id - The UUID of the user
+    # * :opts - Defaults to an empty hash
+    def initialize(**args)
+      @time = args.fetch(:time) { Time.now }
       @finished = false
-      @state = (opts[:state] || :waiting)
-      @query = (opts[:query] || nil)
-      @user_id = (opts[:user_id] || DismalTony::UserIdentity::DEFAULT)
+      @state = args.fetch(:state) { :waiting }
+      @query = (args[:query] || nil)
+      @user_id = args.fetch(:user_id) { nil }
+      @opts = args.fetch(:opts) { {} }
+
+      possible = (('A'..'Z').to_a + ('a'..'z').to_a + ('0'..'9').to_a)
+      @event_id = (0..24).each_with_object([]) { |_n, i| i << possible.sample }.join 
+    end
+
+    # Syntactic sugar, takes a hash +options+ and sets #opts equal to it
+    def with_options(options)
+      @opts = options
     end
 
     # Syntactic sugar for checking the value of @finished
@@ -79,24 +102,44 @@ module DismalTony #:nodoc:
       @finished
     end
 
+    def expiry
+      @expiry ||= @opts.fetch(:expiry) { 60 }
+    end
+
     # Fails the handler and returns false if it's been more than a minute since it was supposed to run
     # Otherwise, returns true if not finished, and our time is valid according to #=~
     def ready?
-      failed if (@time + 60) < Time.now
-      !finished? && self =~ Time.now
+      return failed if (time + expiry) < Time.now
+      !finished? && self < Time.now
     end
 
     # Compares a ScheduleEvent +other+ and self with minute accuracy
     def ==(other)
       comp = other.time
-      comp.year == @time.year && comp.month == @time.month && comp.day == @time.day && comp.hour == @time.hour && comp.min == @time.min
+      comp.to_i == @time.to_i
     end
 
     # Compares a ScheduleEvent to a Time object +other+ with minute accuracy (or uses #== if it's a ScheduleEvent)
     def =~(other)
-      other == self if other.is_a? ScheduleEvent
+      return other == self if other.is_a? ScheduleEvent
       if other.is_a? Time
-        other.year == @time.year && other.month == @time.month && other.day == @time.day && other.hour == @time.hour && other.min == @time.min
+        other.to_i == @time.to_i
+      end
+    end
+
+    def >(other)
+      if other.is_a? Time
+        @time.to_i > other.to_i
+      else
+        nil
+      end
+    end
+
+    def <(other)
+      if other.is_a? Time
+        @time.to_i < other.to_i
+      else
+        nil
       end
     end
 
@@ -106,38 +149,70 @@ module DismalTony #:nodoc:
       other.user_identity == user_identity
     end
 
-    # Simple hash function just off of @time and @user_id
+    # Simple hash function just off of @time, @query, and @user_id
     def hash
       code = 17
       code = 37 * code + @time.hash
       code = 37 * code + @user_id.hash
+      code = 37 * code + @query.hash
     end
 
-    # Changes our #state to :failure and returns false
-    def failed
-      @state = :failure
-      false
-    end
-
-    # Executes this ScheduleEvent using the provided +vi+
-    def run(vi = DismalTony::VIBase.new)
+    # Executes this ScheduleEvent,
+    def run(vi=nil)
       unless finished?
+        vi = construct_vi
         @finished = true
         @state = :success
-        if @conversation_state
-          @user_id.modify_state!(@conversation_state)
-          vi.call('', @user_id, true)
-        elsif @query
-          vi.call(@query, @user_id, true)
-        else
-          DismalTony::HandledResponse.error.with_format(quiet: true)
-        end
+        
+        vi.call(@query)
+        update_me
       end
+      self
+    end
+
+    def call
+      ready? 
+      run
     end
 
     # Simple output of the event
     def to_s
       "(#{@time.strftime('%m/%d/%Y %I:%M%p')}): #{@query if @query}#{@state if @state}"
+    end
+
+    private
+
+    def construct_vi
+      ocon = {}
+
+      if @user_id
+        ocon[:user] = DismalTony.().data_store.select_user(@user_id)
+      end
+
+      if @opts.fetch(:return_interface) { false }
+        ocon[:return_interface] = @opts.fetch(:return_interface)
+      end
+
+      if @opts.fetch(:quiet) { false }
+        ocon[:return_interface] = DismalTony::NowhereInterface.new
+      end
+
+      # if @opts.fetch(:) { false }
+      # end
+
+      DismalTony::VIBase.inherit(ocon)
+    end
+
+    def update_me
+      Scheduler.store_event(self)
+    end
+
+    # Changes our #state to :failure and returns false
+    def failed
+      @finished = true
+      @state = :failure
+      update_me
+      true
     end
   end
 end
