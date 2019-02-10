d10e383f5d70f13ff5b19d8988ca4473abd91d5f
Justin Piotroski <justin.piotroski@gmail.com>
2019-02-09 22:36:22 -0800

    swapped where the text and greeting directives live

--- lib/dismaltony/directives/conversation.rb
+++ lib/dismaltony/directives/conversation.rb
@@ -1,45 +1,62 @@
 require 'dismaltony/parsing_strategies/parsey_parse_strategy'
 
 module DismalTony::Directives
-  class GreetingDirective < DismalTony::Directive
-    include DismalTony::DirectiveHelpers::ConversationHelpers
-    include DismalTony::DirectiveHelpers::EmojiHelpers
-
-    set_name :hello
+  class SendTextMessageDirective < DismalTony::Directive
+    include DismalTony::DirectiveHelpers::DataRepresentationHelpers
+    set_name :send_text
     set_group :conversation
+
+    add_param :sendto
+    add_param :sendmsg
 
     use_parsing_strategies do |use|
       use << DismalTony::ParsingStrategies::ParseyParseStrategy
     end
 
     add_criteria do |qry|
-      qry << must { |q| q.contains?(/hello/, /\bhi\b/, /greetings/) }
-      qry << should { |q| !q['rel', 'discourse'].empty? }
-      qry << could { |q| !q['xpos', 'UH'].empty? }
+      qry << uniquely { |q| q =~ /text/i }
+      qry << should { |q| q.root =~ /text/i }
+      qry << should { |q| q.children_of(q.verb).any? { |w| w.pos == 'NUM' } }
+      qry << could { |q| q =~ /send/i }
+      qry << could { |q| q =~ /message/i }
+    end
+
+    def get_tel
+      parameters[:sendto] = query.raw_text
+      DismalTony::HandledResponse.then_do(directive: self, method: :get_msg, message: '~e:speechbubble Okay, what shall I say to them?', parse_next: false, data: parameters)
+    end
+
+    def get_msg
+      parameters[:sendmsg] = query.raw_text
+      vi.say_through(DismalTony::SMSInterface.new(parameters[:sendto]), parameters[:sendmsg])
+      DismalTony::HandledResponse.finish('~e:speechbubble Okay! I sent the message.')
     end
 
     def run
-      if /how are you/.match?(query)
-        DismalTony::HandledResponse.finish("~e:thumbsup I'm doing well!")
+      resp = nil
+      parameters[:sendto] = query['pos', 'NUM'].first&.to_s
+      if parameters[:sendto].nil?
+        parameters[:sendto] = if /^me$/i.match?(query.children_of(query.verb)&.first)
+                                query.user[:phone]
+                              elsif /[a-z]*/i.match?(query.children_of(query.verb)&.first)
+                                # Code for if it's a name
+                                nil
+                              end
       else
-        moj =
-          random_emoji(
-            'wave',
-            'smile',
-            'rocket',
-            'star',
-            'snake',
-            'cat',
-            'octo',
-            'spaceinvader'
-          )
-        resp =
-          "#{synonym_for('hello').capitalize}#{if (0..100).to_a.sample < 75
-            ', ' + query.user['nickname']
-          end}#{['!', '.'].sample}"
-        DismalTony::HandledResponse.finish("~e:#{moj} #{resp}")
+        resp = DismalTony::HandledResponse.finish("~e:frown That isn't a valid phone number!") if (parameters[:sendto] =~ /^\d{10}$/).nil?
+                          end
+
+      return resp if resp
+
+      if parameters[:sendto].nil?
+        resp = DismalTony::HandledResponse.then_do(directive: self, method: :get_tel, message: '~e:pound Okay, to what number should I send the message?', parse_next: false, data: parameters) if parameters[:sendto].nil?
+      else
+        parameters[:sendmsg] = query.raw_text.split(query['pos', 'VERB'].select { |w| w.any_of?(/says/i, /say/i) }.first.to_s << ' ')[1]
+        vi.say_through(DismalTony::SMSInterface.new(parameters[:sendto]), parameters[:sendmsg])
+        resp = DismalTony::HandledResponse.finish('~e:thumbsup Okay! I sent the message.')
       end
-    end
+      resp
+                        end
   end
 
   class InteractiveSignupDirective < DismalTony::Directive
