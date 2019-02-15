d053cb207277b7bb61ee1a71d5b6efccac50cabf
Justin Piotroski <justin.piotroski@gmail.com>
2019-02-15 00:53:32 -0800

    Modified the matchers / logic for the DiceRoll directive

--- lib/dismaltony/directives/dice.rb
+++ lib/dismaltony/directives/dice.rb
@@ -4,14 +4,11 @@ require 'gaming_dice'
 module DismalTony::Directives
   class DiceRollDirective < DismalTony::Directive
     include DismalTony::DirectiveHelpers::DataRepresentationHelpers
+    include DismalTony::DirectiveHelpers::EmojiHelpers
+
     set_name :diceroll
     set_group :fun
 
-    expect_frags :result
-
-    use_parsing_strategies do |use|
-      use << DismalTony::ParsingStrategies::ComprehendSyntaxStrategy
-    end
 
     add_criteria do |qry|
       qry << uniquely { |q| q =~ /\broll\b/i }
@@ -20,17 +17,29 @@ module DismalTony::Directives
 
     def run
       rolls = query.raw_text.scan(/(((a |\d+)d(\d+)(e)?(\+\d+|\-\d+)?)( \& | \+ )*)/i)
-      frags[:result] = rolls.map! { |r| GamingDice.roll(r[0]) }
-      result_string = if rolls.length == 1
-                        rolls.first
-                      else
-                        rolls.join(', ')
-      end
+      
+      dice_result = rolls.map! { |r| GamingDice.roll(r[0]) }
+      return didnt_work if malformed?(dice_result)
+
+      result_string = rolls.one? ? rolls.first : rolls.join(', ')
+
 
       resp = '~e:dice '
-      resp << "Okay! The result#{rolls.length == 1 ? ' is' : 's are'}: #{result_string}."
-      return_data(frags[:result])
+      resp << "Okay! The result#{rolls.one? ? ' is' : 's are'}: #{result_string}."
+      
+      return_data(dice_result.one? ? dice_result.first : dice_result)
+      
       DismalTony::HandledResponse.finish(resp)
+    end
+
+    private
+
+    def malformed?(dr)
+      dr.one? && dr.first.is_a?(Array) && dr.first.empty?
+    end
+
+    def didnt_work
+      DismalTony::HandledResponse.finish("~e:#{negative_emoji} Sorry, I couldn't parse that dice string.")
     end
   end
 end
