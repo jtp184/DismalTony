e8c221aa1c7d9f8e59ff98eb550b8d61f6501393
Justin Piotroski <justin.piotroski@gmail.com>
2019-02-15 00:52:52 -0800

    Modified the drinkmix matchers and returned a data rep of the drink string

--- lib/dismaltony/directives/fun.rb
+++ lib/dismaltony/directives/fun.rb
@@ -2,6 +2,8 @@ require 'dismaltony/parsing_strategies/aws_comprehend_strategy'
 
 module DismalTony::Directives
   class DrinkMixDirective < DismalTony::Directive
+    include DismalTony::DirectiveHelpers::DataRepresentationHelpers
+
     set_name :drinkmix
     set_group :fun
 
@@ -12,15 +14,14 @@ module DismalTony::Directives
     end
 
     add_criteria do |qry|
-      qry << uniquely { |q| q =~ /drink/i } 
+      qry << must { |q| q =~ /drink/i } 
       qry << must { |q| q.verb&.any_of?(/want/i, /like/i, /mix/i, /make/i, /pick/i, /choose/i) }
-      qry << should { |q| q =~ /please/i }
     end
 
     def run
       frags[:drink] = ['Glass of Water', 'Old Fashioned', 'Zombie', 'Mai Tai', 'Rum & Coke', 'Gin & Tonic', 'Pepsi Crystal'].sample
       moj = %w[martini pineapple think tropicaldrink beer cheers toast champagne].sample
-
+      return_data(frags[:drink])
       DismalTony::HandledResponse.finish("~e:#{moj} Okay, #{query.user['nickname']}. Have a #{frags[:drink]}!")
     end
   end
